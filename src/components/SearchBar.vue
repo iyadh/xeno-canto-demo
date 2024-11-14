23542f6d849efb176fa067e5e4df63e18d1a55f9
Mohamed Gallah <mohamed.gallah@gmail.com>
2024-11-14 15:11:42 +0100

    ✨ SearchBar implementation
    - Add user input
    - Link the input to the store
    - Add filter switch with no effect
    - UI is WIP

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>A search bar shall be shown here ... 🔎</h1>
+  <div>
+    <v-text-field
+      :loading="isLoading"
+      v-model="searisLoadingQuery"
+      clearable
+      label="Search Birds"
+      @keyup.enter="fetchBirds"
+      append-inner-icon="mdi-magnify"
+      @click:append-inner="fetchBirds"
+    ></v-text-field>
+    <v-switch
+      color="primary"
+      v-model="model"
+      :label="`Recs longer than 30s? ${model}`"
+      false-value="No"
+      true-value="Yes"
+      hide-details
+    ></v-switch>
+  </div>
 </template>
+<script setup lang="ts">
+import { useBirdStore } from "@/stores/app";
+import { storeToRefs } from "pinia";
+
+const store = useBirdStore();
+const { searchQuery, isLoading } = storeToRefs(store),
+  { fetchBirds } = store;
+const model = ref("No");
+</script>
