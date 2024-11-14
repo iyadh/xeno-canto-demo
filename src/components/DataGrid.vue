75e10a7e4f128d5255349e8839536fc8af37e495
Mohamed Gallah <mohamed.gallah@gmail.com>
2024-11-14 14:36:05 +0100

    💄Link data grid with store

--- src/components/DataGrid.vue
+++ src/components/DataGrid.vue
@@ -1,3 +1,27 @@
 <template>
-  <h2>DataGrid here as well ..</h2>
+  <v-data-table :headers="headers" :items="birdStore.recordings"></v-data-table>
 </template>
+
+<script setup lang="ts">
+import { useBirdStore } from "@/stores/app";
+
+const birdStore = useBirdStore();
+const searchQuery = ref("grus");
+
+const fetchBirds = () => {
+  birdStore.fetchBirds(searchQuery.value);
+};
+
+const headers = computed(() => [
+  { title: "ID", key: "id", width: 70 },
+  { title: "Genus", key: "gen" },
+  { title: "Species", key: "sp" },
+  { title: "English Name", key: "en" },
+  { title: "Recordist", key: "rec" },
+  { title: "Country", key: "cnt" },
+  { title: "Date", key: "date" },
+  { title: "Audio", key: "file", sortable: false },
+]);
+
+onMounted(() => fetchBirds());
+</script>
