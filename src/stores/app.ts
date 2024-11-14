d8a3ed3d0c769510cbb89fd0e9def5cde3bc7aee
Mohamed Gallah <mohamed.gallah@gmail.com>
2024-11-14 15:09:43 +0100

    ♻️ Update the store
    - Add searchQuery to handle user input
    - Update fetchBirds to handle store's searchQuery instead of handling function params

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -4,18 +4,20 @@ import { Recording, ApiResponse } from "@types";
 
 export const useBirdStore = defineStore("birdsStore", () => {
   const recordings = ref<Recording[]>([]),
+    searchQuery = ref("grus"),
     isLoading = ref(false),
     error = ref<Error | null>(null),
     page = ref(1),
     numPages = ref(1);
+  // lengthFilter = ref<"all" | "lessThan30" | "greaterThan30">("all");
 
-  const fetchBirds = async (query: string) => {
+  const fetchBirds = async () => {
     isLoading.value = true;
     error.value = false;
 
     try {
       const response = await fetch(
-        `https://xeno-canto.org/api/2/recordings?query=${encodeURIComponent(query)}`,
+        `https://xeno-canto.org/api/2/recordings?query=${encodeURIComponent(searchQuery.value)}`,
       );
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -34,6 +36,7 @@ export const useBirdStore = defineStore("birdsStore", () => {
 
   return {
     recordings,
+    searchQuery,
     page,
     numPages,
     isLoading,
